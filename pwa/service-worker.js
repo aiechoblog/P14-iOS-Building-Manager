2419989658adb362dcc601e29a675f71825b7f41
aiechoblog <mr.nazary@gmail.com>
2026-06-21 00:43:43 +0330

    Create service-worker.js

---
+++ pwa/service-worker.js
@@ -0,0 +1,47 @@
+const P14_CACHE_NAME = "p14-pwa-cache-v1";
+const P14_ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./db.js",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(P14_CACHE_NAME).then((cache) => cache.addAll(P14_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== P14_CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(P14_CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
